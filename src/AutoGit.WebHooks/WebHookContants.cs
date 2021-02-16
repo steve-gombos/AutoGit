0cf849d5f1fae13c4d6ee295795cdb43ad1330c4
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2021-02-16 02:19:41 -0500

    Fix code smell

--- src/AutoGit.WebHooks/WebHookContants.cs
+++ src/AutoGit.WebHooks/WebHookContants.cs
@@ -2,6 +2,10 @@
 {
     public class WebHookConstants
     {
+        protected WebHookConstants()
+        {
+        }
+
         public const string EventHeader = "x-github-event";
         public const string DeliveryHeader = "x-github-delivery";
         public const string HubSignatureHeader = "x-hub-signature";
