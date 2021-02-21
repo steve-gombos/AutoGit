f1114f85d047cb34cc4a7c988d1e51889006065f
Steve Gombos <3118886+steve-gombos@users.noreply.github.com>
2021-02-21 03:17:38 -0500

    Add more unit tests

---
+++ tests/AutoGit.WebHooks.UnitTests/WebHookMiddlewareTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoGit.WebHooks.Interfaces;
+using AutoGit.WebHooks.Models.Validators;
+using AutoGit.WebHooks.UnitTests.Fakers;
+using FluentAssertions;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using System.Net;
+using Xunit;
+
+namespace AutoGit.WebHooks.UnitTests
+{
+    public class WebHookMiddlewareTests
+    {
+        private readonly WebHookMiddleware _sut;
+        private readonly RequestDelegate _delegate = Substitute.For<RequestDelegate>();
+
+        public WebHookMiddlewareTests()
+        {
+            _sut = new WebHookMiddleware(_delegate);
+        }
+        
+        [Theory]
+        [InlineData(Constants.ValidWebHookSecret, 1, HttpStatusCode.OK)]
+        [InlineData(Constants.InvalidWebHookSecret, 0, HttpStatusCode.Forbidden)]
+        public async void Invoke_Should_Return_Return_Correct_Status_Code(string secret, int registryCalls, HttpStatusCode statusCode)
+        {
+            // Arrange
+            var fakedWebHookEvent = new WebHookEventFaker().Generate();
+            var fakedHttpContext = new HttpContextFaker().Generate();
+            var mockedWebHookHandlerRegistry = Substitute.For<IWebHookHandlerRegistry>();
+            var mockedWebHookEventFactory = Substitute.For<IWebHookEventFactory>();
+            var mockedWebHookEventValidator = new WebHookEventValidator(Options.Create(new AutoGitWebHookOptions
+                {WebHookSecret = secret}));
+
+            mockedWebHookEventFactory.Create(fakedHttpContext).Returns(fakedWebHookEvent);
+
+            // Act
+            await _sut.Invoke(fakedHttpContext, mockedWebHookHandlerRegistry, mockedWebHookEventFactory,
+                mockedWebHookEventValidator);
+
+            // Assert
+            mockedWebHookEventFactory.Received(1);
+            mockedWebHookHandlerRegistry.Received(registryCalls);
+            fakedHttpContext.Response.StatusCode.Should().Be((int)statusCode);
+        }
+    }
+}
